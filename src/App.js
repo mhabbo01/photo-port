d4689c5edc30c4db28cfa8b6b1a4a5c37863f8d8
Matt Habbo <matthabbo45@gmail.com>
2022-06-16 14:34:49 -0400

    contact test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
+const [contactSelected, setContactSelected] = useState(false);
 
 function App() {
   const [categories] = useState([
@@ -23,13 +24,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>  
+        )}
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
         categories = [],
         setCurrentCategory,
         currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     useEffect(() => {
@@ -27,21 +29,22 @@ function Nav(props) {
           <ul className="flex-row">
             <li className="mx-2">
               <a
-                href="#about" data-testid="about"
+                href="#about" data-testid="about" onClick={() => setContactSelected(false)}
               >
                 About Me
               </a>
             </li>
-            <li>
-              <span>Contact</span>
+            <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+              <span onClick={() => setContactSelected(true)}>Contact</span>
             </li>
             {categories.map((category) => (
               <li className={`mx-1 ${
-                  currentCategory.name === category.name && 'navActive'
+                  currentCategory.name === category.name && !contactSelected && 'navActive'
                   }`} key={category.name}>
                 <span
                   onClick={() => {
-                    setCurrentCategory(category)
+                    setCurrentCategory(category);
+                    setContactSelected(false);
                   }}
                 >
                   {capitalizeFirstLetter(category.name)}
